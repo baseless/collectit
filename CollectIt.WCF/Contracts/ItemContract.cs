31482eee651b58d322a90fdf47f728ed3bbd5d51
Daniel Ryhle <daniel@ryhle.se>
2016-03-13 22:49:20 +0100

    ..

---
+++ CollectIt.WCF/Contracts/ItemContract.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Runtime.Serialization;
+using CollectIt.Common.Entities;
+
+namespace CollectIt.WCF.Contracts
+{
+    [DataContract]
+    public class ItemContract
+    {
+        [DataMember]
+        public string Title { get; set; }
+
+        [DataMember]
+        public string Link { get; set; }
+
+        [DataMember]
+        public string Description { get; set; }
+
+        // ReSharper disable once InconsistentNaming
+        [DataMember]
+        public string PublishedDateRFC822 { get; set; }
+
+        [DataMember]
+        public DateTime? PublishedDate { get; set; }
+
+        public void MapFromItem(Item item)
+        {
+            Title = item.Title;
+            Link = item.Link;
+            Description = item.Description;
+            PublishedDateRFC822 = item.PublishedDateRFC822;
+            DateTime publishedDate;
+            if (!DateTime.TryParse(item.PublishedDate, out publishedDate))
+            {
+                PublishedDate = publishedDate;
+            }
+        }
+    }
+}
---
+++ CollectIt.Web/Models/ViewModels/MemberIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CollectIt.Web.Models.ViewModels
+{
+    public class MemberIndexViewModel
+    {
+    }
+}
