502f726d3dea6f344457234df93fc07a40983268
Daniel Ryhle <daniel@ryhle.se>
2016-03-13 01:41:00 +0100

    ...

---
+++ CollectIt.WCF/Contracts/RSSItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace CollectIt.WCF.Contracts
+{
+    // ReSharper disable once InconsistentNaming
+    [DataContract]
+    public class RSSItem
+    {
+
+    }
+}
