83bad41ec9680626d87312d5238030ce6e114ce7
Daniel Ryhle <daniel@ryhle.se>
2016-03-18 15:36:28 +0100

    xx

--- CollectIt.WCF/ItemService.svc.cs
+++ CollectIt.WCF/ItemService.svc.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.Serialization;
-using System.ServiceModel;
-using System.Text;
 using CollectIt.Common.Entities;
 using CollectIt.Common.Services;
 using CollectIt.WCF.Contracts;
